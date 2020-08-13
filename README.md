d509f04f30cc31e2b62efd7de94fa0bb5f8ad578
AmirAshkan7091 <amirashsd@yahoo.com>
2020-08-13 01:04:24 -0700

    last

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
-# keke
-write by: AmirAshkan7091kekekekek
-# kekek
-write by: fs.appendFile(README.md.process.argv[2]+‘\r\n’,function(error){if…..}
-like this yeah?
+## A Good README generator.
+    
+                    [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+                        
+                    ## table of contents 
+                    * [description](#Description)
+                    * [contribution](#Contribution)  
+                    * [insitillation](#Instillation)
+                    * [usage](#Usage)
+                    * [testing](#Testing)
+                    
+                    ## Description: 
+                    This is my README generator for my homework. Using Node.js I have created a README generator that creates a professional README based on user input generated from Inquirer prompts. 
+                        
+                        
+                    ## Contribution:
+                    all people can to look for it 
+                        
+                    ## Questions?
+                    * **Please check out:** ask me from following:
+                    * **Or you can reach to me on Github here:** [Github](https://github.com/https://github.com/AmirAshkan7091)
+                    * **Im also available via email here:** [Email me!](amirashsd@yahoo.com) 
+
+                    ## Installation 
+                        
+                    For instillation you will need to get this application from my Github and install  the Inquirer package with good knowleg of  Node.js. 
+                        
+                    ## Usage 
+                        
+                     after installing use your knowleg for run the application.the application it is going to ask you some questions that you need to answer.
+                        
+                    ## Testing 
+                        
+                    just do coding and research a lot.
--- index.js
+++ index.js
@@ -1,76 +1,128 @@
+
+const inquirer= require("inquirer");
 const fs = require("fs");
-const inquirer = require("inquirer");
-const questions = [
 
+inquirer.prompt([
     {
-        type: 'input',
-        name: 'repoName',
-        message: 'What is main header?',
-     
+        type: "input",
+        message: "What is the title of your README?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What would you like to put into the description of your README?",
+        name: "description"
+      },
+
+      {
+        type: "input",
+        message: "What are the steps required for installation?",
+        name: "installation"
+      },
+      {
+        type: "input",
+        message: "What are the instructions on how to use this?",
+        name: "usage"
+      },
+      {
+      
+        type: "list",
+        message: "What liscence is this under?",
+        name: "liscence",
+        choices: [
+            `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
+             `[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)`,
+              `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`,
+               `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`]
+      },
+      {
+        type: "input",
+        message: "how can people contribute?",
+        name: "contribution"
+      },
+      {
+        type: "input",
+        message: "What tests have you written for this README?",
+        name: "tests"
+      },
+      {
+        type: "input",
+        message: "Where can people go to ask questions?",
+        name: "questions"
+      },
+      {
+        type: "input",
+        message: "What is your github username?",
+        name: "github"
+      },
+      {
+      type: "input",
+      message: "What is your email?",
+      name: "email"
     },
 
-    {
-        type: 'input',
-        name: 'gitHubName',
-        message: ' username  Github?',
-     
-    },
+]).then(function(response) {
+    const title = response.title;
+    const description = response.description;
+    const contents = response.contents;
+    const installation = response.installation;
+    const usage = response.usage;
+    const liscence = response.liscence;
+    const contribution = response.contribution;
+    const test = response.tests;
+    const questions = response.questions;
+    const github = response.github;
+    const email = response.email;
+    const makeIT = `## ${title}
+    
+${liscence}
+    
+## table of contents 
+* [description](#Description)
+* [contribution](#Contribution)  
+* [insitillation](#Instillation)
+* [usage](#Usage)
+* [testing](#Testing)
 
-    {
-        type: 'input',
-        name: 'description',
-        message: 'description of application',
-     
-    },
- 
-];
+## Description: 
+${description} 
+    
+    
+## Contribution:
+${contribution} 
+    
+## Questions?
+* **Please check out:** ${questions}
+* **Or you can reach to me on Github here:** [Github](https://github.com/${github})
+* **Im also available via email here:** [Email me!](${email}) 
+
+## Installation 
+    
+${installation} 
+    
+## Usage 
+    
+${usage}
+    
+## Testing 
+    
+${test}`; 
+
+
+// write README 
+fs.writeFile("README.MD", makeIT, function(err) {
+        
+    if (err) {
+      return console.log(err);
+    }
+})
+
+})
+
 
 function init() {
-inquirer.prompt(questions).then( (response) =>{
 
-
-    fs.appendFile("README.md", ("# " + response.repoName )+ '\n', function(err) { 
-
-        if (err) { 
-        console.log(err)
-        }
-        else {
-        console.log("Success")
-        }
-    
-    })
-
-fs.appendFile("README.md", ("write by: " + response.gitHubName) + '\n', function(err) { 
-
-    if (err) { 
-    console.log(err)
-    }
-    else {
-    console.log("Success")
-    }
-
-})
-
-fs.appendFile("README.md", ( response.description ) + '\n', function(err) { 
-
-    if (err) { 
-    console.log(err)
-    }
-    else {
-    console.log("Success")
-    }
-
-})
-
-
-})
 }
 
 
 init();
-
-
-// console.log(response)
-// console.log(response.gitHubName)
-// console.log(response.repoName)
-// console.log(response.description)
